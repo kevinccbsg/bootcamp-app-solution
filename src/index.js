af6ec1912ae1dda3a28f62b596253ae32ce196a9
kevin Martínez <kevinccbsg@gmail.com>
2019-04-23 00:53:05 +0200

    Add filter to the beers page

--- src/index.js
+++ src/index.js
@@ -6,8 +6,20 @@ import './images/pilsen-beer/pilsen-beer_lb8drl_c_scale,w_1250.jpg';
 import './images/pilsen-beer/pilsen-beer_lb8drl_c_scale,w_1400.jpg';
 import authRoute from './js/authRoute';
 import { beersTemplate } from './js/beers';
+import { loading } from './js/ui';
 import { getBeers } from './api';
 
 authRoute('/login.html')
   .then(() => getBeers())
   .then(beersTemplate('root'));
+
+const form = document.getElementById('filter-form');
+const filter = document.getElementById('filter');
+const beersContainer = document.getElementById('root');
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  beersContainer.innerHTML = loading();
+  getBeers(filter.value)
+  .then(beersTemplate('root'));
+});
---
+++ src/js/ui.js
@@ -0,0 +1,6 @@
+
+export const loading = () => `
+<div data-cy="login-loading" class="login-loading fa-3x text-center-align">
+  <i class="fas fa-spinner fa-spin"></i>
+</div>
+`;
